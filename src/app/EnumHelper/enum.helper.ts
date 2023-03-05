c2b7e9c9bfa1029a4f1c42b75914be47891c997b
ss <ss@YAZANM_AWAWDA>
2023-03-05 10:42:10 +0200

    Convert enum values to string

--- src/app/EnumHelper/enum.helper.ts
+++ src/app/EnumHelper/enum.helper.ts
@@ -1,6 +1,14 @@
+/* Get values of enum */
+
 export function enumValues (e : object) : any [] {
-  const  mas = Object.values(e) ;
-  return  mas.splice(mas.length / 2 , mas.length);
+  const  mass = Object.values(e) ;
+  return  mass.splice(mass.length / 2 , mass.length);
 
 }
+/* Convert enum values number to string  */
 
+export  function enumToString(enumType : any  , enumValue : any ) : any {
+  for (let enumMember in enumType) {
+    if(enumType[enumMember] == enumValue) return enumMember ;
+  }
+}
--- src/app/Projects/component/project-details/project-details.component.ts
+++ src/app/Projects/component/project-details/project-details.component.ts
@@ -1,10 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GetProject} from "../../../Models/Project/project.model";
 import {ProjectService} from "../../../Services/ProjectServices/project.service";
-import {TaskServices} from "../../../Services/TaskServices/task.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {GetTask} from "../../../Models/Tasks/task.model";
-
+import * as enumModal from "../../../Enum/enum.model";
+import  {enumToString} from "../../../EnumHelper/enum.helper";
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
@@ -13,23 +12,29 @@ import {GetTask} from "../../../Models/Tasks/task.model";
 
 export class ProjectDetailsComponent implements  OnInit {
 
-  projects_ !: GetProject ;
-  /* @Input()*/ projectID !: number  ;
+  public projects_ !: GetProject ;
+  /* @Input()*/ public projectID !: number  ;
   // @Output() projectIDChanged = new EventEmitter<number>(); // declare the event emitter here
 
+  public currentStatus !: enumModal.ProjectStatus[] | any ;
   constructor(private  projectService : ProjectService ,
               private route : ActivatedRoute ,
               private  router : Router) {
   }
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.projectID = +params['id'];
-      this.projectService.getProjectByID((this.projectID)).subscribe((project_:GetProject   )  => {
-        this.projects_ = project_ ;
-         console.log(project_);
-        // this.projectIDChanged.emit(this.projectID); // emit the event when projectID is updated
 
-      });
+  ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.projectID = +params['id'];});
+
+      this.projectService.getProjectByID((this.projectID)).subscribe(
+        (project_:GetProject   ) => {
+          this.projects_ = project_;
+          let e = enumToString(enumModal.ProjectStatus , this.projects_.currentStatus ) ;
+          this.projects_.currentStatus = e ;
+          console.log(e);
+          // this.projects_.currentStatus = enumModal.ProjectStatus[this.currentStatus]
+         // this.projectIDChanged.emit(this.projectID); // emit the event when projectID is updated
     });
   }
 
